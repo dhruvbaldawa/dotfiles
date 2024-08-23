0ba67d5c5fe17bba08eee59c3ec63c1c9473e265
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2024-08-23 21:47:12 +0400

    update config

--- home/zshrc.sh
+++ home/zshrc.sh
@@ -13,10 +13,18 @@ fi
 export HISTSIZE=9999999
 export HISTFILESIZE=9999999
 export SAVEHIST=${HISTSIZE}
-setopt INC_APPEND_HISTORY
 export HISTTIMEFORMAT="[%F %T] "
-setopt HIST_IGNORE_ALL_DUPS
-unsetopt SHARE_HISTORY             # Share history between all sessions.
+setopt EXTENDED_HISTORY      # Write the history file in the ':start:elapsed;command' format.
+setopt INC_APPEND_HISTORY    # Write to the history file immediately, not when the shell exits.
+unsetopt SHARE_HISTORY       # Share history between all sessions.
+setopt HIST_IGNORE_DUPS      # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS  # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_IGNORE_SPACE     # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS     # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY           # Do not execute immediately upon history expansion.
+setopt APPEND_HISTORY        # append to history file (Default)
+setopt HIST_NO_STORE         # Don't store history commands
+setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from each command line being added to the history.
 
 # Install fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -58,7 +66,7 @@ antigen bundle MichaelAquilina/zsh-you-should-use
 antigen bundle changyuheng/fz
 antigen bundle zsh-users/zsh-completions
 antigen bundle dbz/kube-aliases
-antigen bundle lukechilds/zsh-nvm
+# antigen bundle lukechilds/zsh-nvm
 antigen apply
 
 [[ -f "$HOME/.aliases" ]] && source $HOME/.aliases
