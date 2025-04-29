ad114458be3f210575d098436b752c26c52afcae
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2025-04-29 19:37:42 +0400

    Setup gopass and git settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-curl https://raw.githubusercontent.com/dhruvbaldawa/dotfiles/refs/heads/main/files/brew/Brewfile | brew bundle --file=- --no-lockfile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
+curl https://raw.githubusercontent.com/dhruvbaldawa/dotfiles/refs/heads/main/files/brew/Brewfile | brew bundle --file=- && \
 chezmoi init --ssh dhruvbaldawa/dotfiles
 # chezmoi
 # starship
