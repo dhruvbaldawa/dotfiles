250c14c88c93ca1a736cde2b92b7c25c8c01e4b3
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2025-04-30 00:38:40 +0400

    fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
 curl https://raw.githubusercontent.com/dhruvbaldawa/dotfiles/refs/heads/main/files/brew/Brewfile | brew bundle --file=- && \
-chezmoi init --ssh dhruvbaldawa/dotfiles
+gopass clone git@github.com:dhruvbaldawa/secrets.git && \
+gopass sync && \
+# Export the GPG public key to the clipboard
+echo "Please share the GPG public key with me to continue the installation." && \
+chezmoi init --ssh dhruvbaldawa/dotfiles && \
+chezmoi apply
 # chezmoi
 # starship
 # nushell
