5578f254857404845d0d5fa7cabdfb09d572f9c2
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2025-12-16 15:04:43 +0530

    rename to plan

--- tools.sh
+++ tools.sh
@@ -11,6 +11,6 @@ export OTEL_LOGS_EXPORTER=otlp
 export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
 export OTEL_EXPORTER_OTLP_ENDPOINT=https://otel.dhruv.cc
 export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Bearer ${OTEL_BEARER_TOKEN}"
-export OTEL_RESOURCE_ATTRIBUTES=source=subscription
+export OTEL_RESOURCE_ATTRIBUTES=plan=claude-max
 export OTEL_METRIC_EXPORT_INTERVAL=10000
 export OTEL_LOGS_EXPORT_INTERVAL=5000
